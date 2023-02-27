befcc1c630660f07d220ee3ea488a70e9548f2ee
Felix Lindblom <life22ku@student.ju.se>
2023-02-28 00:55:01 +0100

    0.2.0 small steps foward

--- lunar.js
+++ lunar.js
@@ -1,23 +1,38 @@
 
 console.log('LINK UP')
 
+
 function setup() {
   createCanvas(500, 700);
 }
 
 function draw() {
-  background(220);
-  
+  background(20);
+  let faceCol = color(140,70,200)
+  let eyebrowCol = color(255,255,255)
+  let missingNoseCol = color(20,20,20)
+
   
   push();
     translate(250,400)
+    fill(faceCol)
     ellipse(0,0,275,290);
-    fill(100,70,200)
+
+    //eyebrow
     beginShape();
       noFill()
+      stroke(eyebrowCol)
       strokeWeight(3)
-      arc(-50,-70,80,50,PI, 0)
+      arc(-50,-50,90,45,PI, -0.2)
     endShape();
+
+
+    // nose
+    beginShape()
+        stroke(missingNoseCol)
+        vertex(0,-20)
+        bezier(0,-20,-70,7,-40,50,-10,40)
+    endShape()
 
   pop();
 
