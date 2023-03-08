80907c6d1954adce396feaecd0d3ed04b20f0003
Felix Lindblom <life22ku@student.ju.se>
2023-03-08 02:05:47 +0100

    0.4.0 buggy but playable

--- lunar.js
+++ lunar.js
@@ -1,29 +1,64 @@
-
-console.log('LINK UP')
-
-
-
-
-  
-  function setup() {
+function setup() {
     createCanvas(500, 700);
     frameRate(60);
-  }
+    background(20);
+    
+}
   // Fall total speed
   let fallTot = 0
   // acceleration total
   let accTot = 0.1
 
-  let won = 0
+  let pauseStatus = 0
   let smileStart = 0
   let smileEnd = 0
 
+  let noseCol = 20
+  flipNoseCol = 1
+
+  startScreen = true
+
   function draw() {
-    background(20);
     let faceCol = color(140,70,200)
-    let eyebrowCol = color(255,255,255)
+    let gameWhite = color(255,255,255)
     let missingNoseCol = color(20,20,20)
 
+    if(startScreen === true){
+      push();
+      // Start button
+      beginShape();
+      fill(255)
+      textAlign(CENTER);
+      textFont('Verdana',70)
+      text('Start', 255,130);
+      textStyle(NORMAL);
+      endShape();
+
+      if(mouseX < 350 && mouseX > 170 && mouseY < 130 && mouseY > 70 && mouseIsPressed === true){
+        erase();
+        startScreen = false
+      }
+
+  
+        //Game name
+        beginShape();
+          fill(noseCol)
+          textAlign(CENTER);
+          textFont('Times New Roman',70)
+          text('Nose', 255,530);
+          
+          noseCol +=1*flipNoseCol
+          if(noseCol===255){
+            flipNoseCol = -1
+          }if(noseCol === 20){
+            flipNoseCol = 1
+          }
+          
+        endShape();
+        pop();
+    }else{
+
+    
     
     push();
       translate(250,400)
@@ -45,7 +80,7 @@ console.log('LINK UP')
       //eyebrow left
       beginShape();
         noFill()
-        stroke(eyebrowCol)
+        stroke(gameWhite)
         strokeWeight(3)
         arc(-65,-50,90,45,PI, -0.2,)
       endShape();
@@ -53,7 +88,7 @@ console.log('LINK UP')
       //eyebrow right
       beginShape();
         noFill()
-        stroke(eyebrowCol)
+        stroke(gameWhite)
         strokeWeight(3)
         arc(65,-50,90,45,PI+0.2, 0)
       endShape();
@@ -79,17 +114,23 @@ console.log('LINK UP')
         endShape();
       };
 
-      function redoText(inText){
+      function redoText(inText,maxMouseX,minMouseX,maxMouseY,minMouseY){
         beginShape();
-          fill(eyebrowCol)
+          fill(gameWhite)
           textAlign(CENTER);
           textFont('Verdana',70)
           text(inText, 0, -250);
         endShape();
+        if(mouseX < maxMouseX && mouseX > minMouseX && mouseY < maxMouseY && mouseY > minMouseY && mouseIsPressed === true){
+          fallTot = 0
+          accTot = 0.1
+          pauseStatus = 0
+        }
+
       }
       
       if(accTot < 0.1 && accTot > -0.1 && -200+fallTot >-5 &&-200+fallTot <5 ){
-        won = 1
+        pauseStatus = 1
         fallTot =200
         smile(smileStart,smileEnd);
         if(smileStart < 0.3){
@@ -98,37 +139,67 @@ console.log('LINK UP')
         if(smileEnd <PI -0.3){
           smileEnd += 0.1
         }
-        redoText('Redo?')
+        redoText('Redo?',350,140,150,95)
       };
-      
-      if(won === 0 ){
+      if(mouseIsPressed === true){
+        console.log(mouseX)
+      }
+      if(pauseStatus === 0 ){
         if (fallTot<350){
         fallTot += 0.2*accTot
         accTot += 0.3
         }else{
-          fallTot = 0
-          accTot = 0.1
+          redoText('Retry',350,155,165,95)
         }
         if (keyIsPressed && keyCode === UP_ARROW) {
           accTot -= 1
         }
       }
-      
-      
-      
-      
-  
-      
+
+
       noseMaker(0,0,missingNoseCol)
-      noseMaker(0,-200+fallTot,eyebrowCol)
-
-
-      
-
-      
-
-      
+      noseMaker(0,-200+fallTot,gameWhite)
 
     pop();
 
   };
+  }
+  // function draw(){
+  //   if(mouseIsPressed === true){
+  //     console.clear()
+  //     console.log(mouseX + 'x')
+  //     console.log(mouseY + 'Y')
+      
+  //   }
+    
+  //   // Start button
+  //   beginShape();
+  //     fill(255)
+  //     textAlign(CENTER);
+  //     textFont('Verdana',70)
+  //     text('Start', 255,130);
+  //     textStyle(NORMAL);
+  //   endShape();
+
+  //   if(mouseX < 350 && mouseX > 170 && mouseY < 130 && mouseY > 70 && mouseIsPressed === true){
+  //     startScreen = fa
+  //   }
+
+    
+  //   //Game name
+  //   beginShape();
+  //     fill(noseCol)
+  //     textAlign(CENTER);
+  //     textFont('Times New Roman',70)
+  //     text('Nose', 255,530);
+      
+  //     noseCol +=1*flipNoseCol
+  //     if(noseCol===255){
+  //       flipNoseCol = -1
+  //     }if(noseCol === 20){
+  //       flipNoseCol = 1
+  //     }
+      
+  //   endShape();
+    
+  // }
